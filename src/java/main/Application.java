31247cbc85dbebc45cea927e242ee5d0e0e83124
Aiat Baizhigitov <ayatbaizi@gmail.com>
2022-10-17 13:29:16 +0600

    Implementation of verification of the entered command

--- src/java/main/Application.java
+++ src/java/main/Application.java
@@ -15,52 +15,68 @@ public class Application {
 
         Scanner sc = new Scanner(System.in);
         List<Animal> animals = new ArrayList<>();
+
 //menu commands: add, list, exit
         {
             while (true) {
+                boolean check = false;
                 System.out.println("Введите команду(add/list/exit): ");
-                Commands userCommand = Commands.valueOf(sc.next().toUpperCase().trim());
-                switch (userCommand) {
-                    case ADD:
-                        System.out.println("Какое животное(cat/dog/duck) добавить?");
-                        String animalType = sc.next();
-                        Animal animal = null;
-                        System.out.println("Какое имя у животного?");
-                        String name = sc.next();
-                        System.out.println("Какой возраст у животного?");
-                        int age = sc.nextInt();
-                        System.out.println("Какой вес животного?");
-                        int weight = sc.nextInt();
-                        System.out.println("Укажите цвет животного?");
-                        String color = sc.next();
-                        if (animalType.equals("cat")) {
-                            animal = new Cat(name, age, weight, color);
-                        } else if (animalType.equals("dog")) {
-                            animal = new Dog(name, age, weight, color);
-                        } else if (animalType.equals("duck")) {
-                            animal = new Duck(name, age, weight, color);
-                        }
-                        animal.say();
-                        animals.add(animal);
-                        if (animal instanceof Duck){
-                            ((Duck) animal).fly();
-                        }
+                String userInput = sc.next();
+                for (Commands commands : Commands.values()) {
+                    if (userInput.equalsIgnoreCase(commands.name())) {
+                        check = true;
                         break;
-                    case LIST:
-                        for (Animal anim : animals) {
-                            System.out.println(anim);
-                        }
-                        break;
-                    case EXIT:
-                        System.out.println("Программа заверешена");
-                        System.exit(0);
-                    default:
-                        System.out.println("Неверная команда, используйте add, list, exit");
+                    }
+                }
+                if (!check) {
+                    System.out.println("Неверная команда! Введите корректную команду!");
+                } else {
+                    Commands userCommand = Commands.valueOf(userInput.toUpperCase().trim());
+                    switch (userCommand) {
+                        case ADD:
+                            System.out.println("Какое животное(cat/dog/duck) добавить?");
+                            String animalType = sc.next();
+                            Animal animal = null;
+                            System.out.println("Какое имя у животного?");
+                            String name = sc.next();
+                            System.out.println("Какой возраст у животного?");
+                            int age = sc.nextInt();
+                            System.out.println("Какой вес животного?");
+                            int weight = sc.nextInt();
+                            System.out.println("Укажите цвет животного?");
+                            String color = sc.next();
+                            switch (animalType) {
+                                case "cat" -> animal = new Cat(name, age, weight, color);
+                                case "dog" -> animal = new Dog(name, age, weight, color);
+                                case "duck" -> animal = new Duck(name, age, weight, color);
+                            }
+                            animal.say();
+                            animals.add(animal);
+                            if (animal instanceof Duck) {
+                                ((Duck) animal).fly();
+                            }
+                            break;
+                        case LIST:
+                            for (Animal anim : animals) {
+                                System.out.println(anim);
+                            }
+                            break;
+                        case EXIT:
+                            System.out.println("Программа заверешена");
+                            System.exit(0);
+                    }
                 }
             }
-        }
 
+        }
     }
 }
+
+
+
+
+
+
+
 
 
